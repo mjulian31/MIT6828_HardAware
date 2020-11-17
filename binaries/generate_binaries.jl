5068dd1d14f7cc1648d546f071312d1bc1290768
Meredith Julian <mjulian@mit.edu>
2020-11-16 20:56:29 -0500

    assembly cpu only

--- binaries/generate_binaries.jl
+++ binaries/generate_binaries.jl
@@ -120,7 +120,7 @@ for i in 1:size(ARGS)[1]
     a = rand(DIM, DIM)
     b = rand(DIM, DIM)
     c = zeros(DIM, DIM)
-    outfile = string("cpu_", DIM, ".ll")
+    outfile = string("cpu_", DIM, ".s")
     open(outfile, "w") do out
         redirect_stdout(out) do
             @code_native debuginfo=:none dump_module=true mul_tile!(a, b, c)
@@ -128,17 +128,17 @@ for i in 1:size(ARGS)[1]
     end
     println("done. saved cpu binary to ", outfile)
 
-    print("generating gpu binary...")
-    a = CUDA.rand(DIM, DIM)
-    b = CUDA.rand(DIM, DIM)
-    c = CUDA.zeros(DIM, DIM)
-    kern = coalesced_matmul_kernel!(CUDADevice(), (TILE_DIM, TILE_DIM))
-    outfile = string("gpu_", DIM, ".ll")
-    open(outfile, "w") do out
-        redirect_stdout(out) do
-            CUDA.@device_code_native debuginfo=:none dump_module=true kern(c, a, b, ndrange=size(c))
-        end
-    end
-    println("done. saved gpu binary to ", outfile)
+    # print("generating gpu binary...")
+    # a = CUDA.rand(DIM, DIM)
+    # b = CUDA.rand(DIM, DIM)
+    # c = CUDA.zeros(DIM, DIM)
+    # kern = coalesced_matmul_kernel!(CUDADevice(), (TILE_DIM, TILE_DIM))
+    # outfile = string("gpu_", DIM, ".s")
+    # open(outfile, "w") do out
+    #     redirect_stdout(out) do
+    #         CUDA.@device_code_native debuginfo=:none dump_module=true kern(c, a, b, ndrange=size(c))
+    #     end
+    # end
+    # println("done. saved gpu binary to ", outfile)
 
 end
