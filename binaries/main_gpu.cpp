9342014038bbfa575547e618173fc8280b701d72
Meredith Julian <mjulian@mit.edu>
2020-11-18 00:12:08 -0500

    fix

--- binaries/main_gpu.cpp
+++ binaries/main_gpu.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
   cuModuleLoad(&module, "matmul_gpu.ptx");
 
   CUfunction kernel_addr;
-  checkCudaErrors(cuModuleGetFunction(&kernel_addr, module, "_Z12julia_matmul3PtrI7Float64ES_IS0_ES_IS0_E5Int32"));
+  cuModuleGetFunction(&kernel_addr, module, "_Z12julia_matmul3PtrI7Float64ES_IS0_ES_IS0_E5Int32");
 
   // Print the vector length to be used, and compute its size
   int dim = 1024;
@@ -47,21 +47,21 @@ int main(int argc, char **argv) {
 
   // Allocate the device input vector A
   CUdeviceptr d_A;
-  checkCudaErrors(cuMemAlloc(&d_A, size));
+  cuMemAlloc(&d_A, size);
 
   // Allocate the device input vector B
   CUdeviceptr d_B;
-  checkCudaErrors(cuMemAlloc(&d_B, size));
+  cuMemAlloc(&d_B, size);
 
   // Allocate the device output vector C
   CUdeviceptr d_output;
-  checkCudaErrors(cuMemAlloc(&d_output, size));
+  cuMemAlloc(&d_output, size);
 
   // Copy the host input vectors A and B in host memory to the device input
   // vectors in device memory
   printf("Copy input data from the host memory to the CUDA device\n");
-  checkCudaErrors(cuMemcpyHtoD(d_A, h_A, size));
-  checkCudaErrors(cuMemcpyHtoD(d_B, h_B, size));
+  cuMemcpyHtoD(d_A, h_A, size);
+  cuMemcpyHtoD(d_B, h_B, size);
 
   // Launch the Vector Add CUDA Kernel
   int threadsPerBlock = 32;
@@ -74,24 +74,24 @@ int main(int argc, char **argv) {
   void *arr[] = {reinterpret_cast<double *>(&d_A), reinterpret_cast<double *>(&d_B),
                  reinterpret_cast<double *>(&d_output),
                  reinterpret_cast<int *>(&dim)};
-  checkCudaErrors(cuLaunchKernel(kernel_addr, cudaGridSize.x, cudaGridSize.y,
+  cuLaunchKernel(kernel_addr, cudaGridSize.x, cudaGridSize.y,
                                  cudaGridSize.z, /* grid dim */
                                  cudaBlockSize.x, cudaBlockSize.y,
                                  cudaBlockSize.z, /* block dim */
                                  0, 0,            /* shared mem, stream */
                                  &arr[0],         /* arguments */
-                                 0));
-  checkCudaErrors(cuCtxSynchronize());
+                                 0);
+  cuCtxSynchronize();
 
   // Copy the device result vector in device memory to the host result vector
   // in host memory.
   printf("Copy output data from the CUDA device to the host memory\n");
-  checkCudaErrors(cuMemcpyDtoH(h_output, d_output, size));
+  cuMemcpyDtoH(h_output, d_output, size);
 
   // Free device global memory
-  checkCudaErrors(cuMemFree(d_A));
-  checkCudaErrors(cuMemFree(d_B));
-  checkCudaErrors(cuMemFree(d_output));
+  cuMemFree(d_A);
+  cuMemFree(d_B);
+  cuMemFree(d_output);
 
   // Free host memory
   free(h_A);
