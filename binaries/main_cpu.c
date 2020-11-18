57e0398c599685f50530330b2c5fcfa43a52f97e
Meredith Julian <mjulian@mit.edu>
2020-11-18 11:54:24 -0500

    error fix

--- binaries/main_cpu.c
+++ binaries/main_cpu.c
@@ -1,6 +1,7 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
 void matmul(double* out, double* A, double* B, int dim);
 
@@ -14,15 +15,15 @@ int main(int argc, char *argv[]) {
 	double* output = malloc(sizeof(double) * dim * dim);
 	for (int i = 0; i < dim; ++i) {
     for (int j = 0; j < dim; ++j) {
-			h_A[dim*i + j] = rand() / static_cast<double>(RAND_MAX);
-      h_B[dim*i + j] = rand() / static_cast<double>(RAND_MAX);
-      h_output[dim*i + j] = 0;
+			A[dim*i + j] = rand() / static_cast<double>(RAND_MAX);
+      B[dim*i + j] = rand() / static_cast<double>(RAND_MAX);
+      output[dim*i + j] = 0;
     }
   }
 	matmul(output, A, B, dim);
 	for (int i = 0; i < dim; ++i) {
     for (int j = 0; j < dim; ++j) {
-      if (h_output[dim*i + j] == 0) {
+      if (output[dim*i + j] == 0) {
         printf("error with output\n");
       }
     }
