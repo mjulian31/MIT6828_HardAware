61a6ccf5f9a7e3f4a7d2001a4f207283343f6ba9
Taylor Andrews <taylorhandrews@gmail.com>
2020-12-02 20:38:23 -0500

    thank you valgrind for finding more memory leaks - fix pipe alloc free

--- src/haw_scheduler/subprocess.cpp
+++ src/haw_scheduler/subprocess.cpp
@@ -145,7 +145,10 @@ ChildHandle* start_subprocess_nonblocking(std::string binpath, std::string args,
        //handle->pipes[PARENT_READ_PIPE][WRITE_FD] = pipes[PARENT_READ_PIPE][WRITE_FD];
     
        printf("SUBPROCESS: freeing argv_list\n");
-       free(pipes);     // free temporary pipes holder now that its in handle
+       // free temporary pipes holder now that its in handle
+       for(int j = 0; j < 2; j++) { delete pipes[j]; }
+       delete pipes;
+
        free(argv_list); // free command line args that were sent
        printf("SUBPROCESS: started successfully\n");
        return handle;
