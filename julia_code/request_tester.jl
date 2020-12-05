a3802459585988b63afee8aa9350a32303196275
Meredith Julian <mjulian@mit.edu>
2020-12-04 20:43:12 -0500

    simple mul correctness test

---
+++ julia_code/request_tester.jl
@@ -0,0 +1,29 @@
+include("request_handler.jl")
+
+# matrix multiplication
+function mult(x, y)
+    req_num, _ = send_request(x, y)
+    while !haskey(responses, req_num)
+        # wait for response to come back
+    end
+    # got response, return matrix
+    return get(responses, req_num, :none)
+end
+
+println("test 1...")
+a = rand(10, 10)
+b = rand(10, 10)
+@assert mult(a, b) == a*b
+println("done.")
+
+println("test 2...")
+a = rand(1024, 1000)
+b = rand(1000, 1020)
+@assert mult(a, b) == a*b
+println("done.")
+
+println("test 3...")
+a = rand(4098, 10)
+b = rand(10, 124)
+@assert mult(a, b) == a*b
+println("done.")
